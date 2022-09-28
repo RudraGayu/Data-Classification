47436c14a96c87ef9c61af108d81eafbda32e039
RudraGayu <83653823+RudraGayu@users.noreply.github.com>
2022-09-28 12:30:44 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+import pandas as pd
+import matplotlib.pyplot as plt
+import statistics as st
+import numpy as np
+from sklearn import decomposition
+import pandas as pd
+import matplotlib.pyplot as plt
+import statistics as st
+import numpy as np
+from sklearn import decomposition
+
+df = pd.read_csv("SteelPlateFaults-2class.csv")
+
+print(df)
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
